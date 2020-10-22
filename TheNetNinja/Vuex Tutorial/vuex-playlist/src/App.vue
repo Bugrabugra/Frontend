39e6d86ad9f2b5f8e721ffd710700c54dd065ea7
Bugrabugra <bugraotken@gmail.com>
2020-10-22 23:22:07 +0300

    22102020 - Vuex %50

---
+++ TheNetNinja/Vuex Tutorial/vuex-playlist/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from "./components/ProductListOne";
+import ProductListTwo from "./components/ProductListTwo";
+
+
+export default {
+  components: {
+    "product-list-one": ProductListOne,
+    "product-list-two": ProductListTwo
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+  body {
+    font-family: Ubuntu, Consolas;
+    color: #555;
+  }
+</style>
---
+++ TheNetNinja/Vuex Tutorial/vuex-playlist/src/components/ProductListTwo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="product-list-two">
+    <h2>Product List Two</h2>
+    <ul>
+      <li v-for="product in products">
+        <span class="name">{{product.name}}</span>
+        <span class="price">{{product.price}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      products() {
+        return this.$store.state.products;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #product-list-two {
+    background: #D1E4FF;
+    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.3);
+    margin-bottom: 30px;
+    padding: 10px 20px;
+  }
+
+  #product-list-two ul {
+    padding: 0;
+    list-style-type: none;
+  }
+
+  #product-list-two li {
+    margin-right: 10px;
+    margin-top: 10px;
+    padding: 20px;
+    background: rgba(255, 255, 255, 0.7);
+  }
+
+  .price {
+    font-weight: bold;
+    color: #860CE8;
+    display: block;
+  }
+</style>
