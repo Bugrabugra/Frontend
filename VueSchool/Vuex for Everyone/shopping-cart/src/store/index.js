03dfb2f6c0fae512c6ae7e35e6b9fb79587fcbbc
Bugrabugra <bugraotken@gmail.com>
2020-10-29 15:14:56 +0300

    29102020 - Vuex eğitimi

---
+++ VueSchool/Vuex for Everyone/shopping-cart/src/store/index.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import actions from "./actions";
+import cart from "./modules/cart";
+import products from "./modules/products";
+
+
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  modules: {
+    "cart": cart,
+    "products": products
+  },
+
+  state: {
+
+  },
+
+  getters: {
+
+  },
+
+  actions: actions,
+
+  mutations: {
+
+  }
+})
