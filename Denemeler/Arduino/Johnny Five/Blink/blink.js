ab67cc0c5e093fbb0a16b63cd9a98b4a2cec8d24
Bugrabugra <bugraotken@gmail.com>
2020-12-10 01:36:48 +0300

    10122020 - Vue.js 3 - Arduino

--- Denemeler/Arduino/Johnny Five/Blink/blink.js
+++ Denemeler/Arduino/Johnny Five/Blink/blink.js
@@ -1,12 +1,17 @@
-const { Board, Led } = require("johnny-five");
+const {Board, Led, Sensor} = require("johnny-five");
 const board = new Board();
 
 board.on("ready", () => {
-
-  // Create a standard `led` component instance
   const led = new Led(13);
+  const potentiometer = new Sensor("A2");
 
-  // "blink" the led in 500ms
-  // on-off phase periods
+  potentiometer.on("change", () => {
+    const {value, raw} = potentiometer;
+    console.log("Sensor: ");
+    console.log("  value  : ", value);
+    console.log("  raw    : ", raw);
+    console.log("-----------------");
+  });
+
   led.blink(500);
 });
