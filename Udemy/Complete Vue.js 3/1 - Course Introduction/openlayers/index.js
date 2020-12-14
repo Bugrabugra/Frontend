eaa43cffea37d93ea536fae32649d75a83c4e478
bugrabugra <bugraotken@gmail.com>
2020-12-14 23:19:52 +0300

    14122020 - Upwork 2

--- Udemy/Complete Vue.js 3/1 - Course Introduction/openlayers/index.js
+++ Udemy/Complete Vue.js 3/1 - Course Introduction/openlayers/index.js
@@ -11,30 +11,33 @@ import TileJSON from 'ol/source/TileJSON';
 import VectorSource from 'ol/source/Vector';
 import Point from 'ol/geom/Point';
 import {Circle, Fill, Style, Icon} from 'ol/style';
-// import {useGeographic} from 'ol/proj';
-//
-// useGeographic();
+import {fromLonLat} from 'ol/proj';
 
-var place = [-62.8417, 140.6421];
 
-var point = new Point(place);
+// var place = [-62.8417, 140.6421];
+const place = [62.8417, 60.6421];
 
-var vectorSource = new VectorSource({
-  features: [new Feature(point)],
-});
 
-var vectorLayer = new VectorLayer({
+const point = new Point(fromLonLat(place));
+
+const vectorSource = new VectorSource();
+
+// var vectorSource = new VectorSource({
+//   features: [new Feature(point)],
+// });
+
+const vectorLayer = new VectorLayer({
   source: vectorSource,
   style: new Style({
     image: new Circle({
-      radius: 9,
-      fill: new Fill({color: 'red'}),
+      radius: 10,
+      fill: new Fill({color: 'black'}),
     }),
   }),
 });
 
 
-var source = new XYZ({
+const source = new XYZ({
   url: "https://gibs-{a-c}.earthdata.nasa.gov/wmts/epsg4326/best/" +
     "GHRSST_L4_AVHRR-OI_Sea_Surface_Temperature/default/2020-04-02/" +
     "2km/{z}/{y}/{x}.png",
@@ -42,7 +45,7 @@ var source = new XYZ({
 });
 
 
-var raster = new RasterSource({
+const raster = new RasterSource({
   sources: [source],
   operation: function (pixels, data) {
     var pixel = pixels[0]; // select from the only source - pixels will be an array of length 1 - corresponding to the only source.
@@ -51,26 +54,26 @@ var raster = new RasterSource({
       pixel[0] = 255;
       pixel[1] = 255;
       pixel[2] = 0;
-
-      // coldPlaces.push(pixel)
     }
-    // if (pixel[0] === 124 && pixel[1] === 8 && pixel[2] === 0) {
-    //   // max temperature pixel.- set yellow.
-    //   pixel[0] = 0;
-    //   pixel[1] = 255;
-    //   pixel[2] = 0;
-    //   console.log("modifying min pixel..");
-    // }
+
+    if (pixel[0] === 124 && pixel[1] === 8 && pixel[2] === 0) {
+      // max temperature pixel.- set yellow.
+      pixel[0] = 0;
+      pixel[1] = 0; //255
+      pixel[2] = 0;
+    }
     return pixel;
   }
 });
 
-var imageLayer = new ImageLayer({ source: raster });
+const imageLayer = new ImageLayer({ source: raster });
 
-var tileLayer = new TileLayer({ source: source });
+
+
+const tileLayer = new TileLayer({ source: source });
 // map.addLayer(tileLayer);
 
-var map = new Map({
+const map = new Map({
   view: new View({
     extent: [-20037508.34, -20037508.34, 20037508.34, 20037508.34],
     center: [0, 0],
