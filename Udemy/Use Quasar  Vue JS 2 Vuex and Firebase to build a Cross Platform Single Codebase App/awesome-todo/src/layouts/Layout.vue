529bad5c467f554e8fc735fa70bf85bc2a754e88
Bugrabugra <bugraotken@gmail.com>
2021-01-22 16:29:02 +0300

    22012021 - Quasar

---
+++ Udemy/Use Quasar  Vue JS 2 Vuex and Firebase to build a Cross Platform Single Codebase App/awesome-todo/src/layouts/Layout.vue
@@ -0,0 +1,86 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="absolute-center">Awesome Todo</q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-footer>
+      <q-tabs>
+        <q-route-tab
+          v-for="nav in navs"
+          :key="nav.label"
+          :to="nav.to"
+          :icon="nav.icon"
+          :label="nav.label"
+        />
+      </q-tabs>
+    </q-footer>
+
+    <q-drawer
+      :breakpoint="767"
+      v-model="leftDrawerOpen"
+      :width="250"
+      bordered
+      content-class="bg-primary"
+    >
+      <q-list dark>
+        <q-item-label header>Navigation</q-item-label>
+
+        <q-item
+          v-for="nav in navs"
+          :key="nav.label"
+          :to="nav.to"
+          class="text-grey-4"
+          exact
+          clickable
+        >
+          <q-item-section avatar>
+            <q-icon :name="nav.icon"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{nav.label}}</q-item-label>
+          </q-item-section>
+        </q-item>
+
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+  export default {
+    name: 'MainLayout',
+    components: {},
+    data () {
+      return {
+        leftDrawerOpen: this.$q.platform.is.desktop,
+        navs: [
+          {label: "Todo", icon: "list", to: "/"},
+          {label: "Settings", icon: "settings", to: "/settings"},
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @media screen and (min-width: 768px){
+    .q-footer {
+      display: none;
+    }
+  }
+
+  .q-drawer {
+    .q-router-link--exact-active {
+      color: white !important;
+    }
+  }
+</style>
---
+++ Udemy/Use Quasar  Vue JS 2 Vuex and Firebase to build a Cross Platform Single Codebase App/awesome-todo/src/pages/PageTodo.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-page class="q-pa-md">
+    <q-list bordered separator>
+      <Task v-for="(task, key) in tasks"
+            :task="task"
+            :key="key"
+            :id="key"/>
+    </q-list>
+
+    <div class="absolute-bottom text-center q-mb-lg">
+      <q-btn
+        @click="showAddTask = true"
+        color="primary"
+        size="24px"
+        icon="add"
+        round
+      />
+    </div>
+
+    <q-dialog v-model="showAddTask">
+      <AddTask/>
+    </q-dialog>
+
+  </q-page>
+</template>
+
+<script>
+  import {mapGetters} from "vuex";
+  import Task from "components/Tasks/Task";
+  import AddTask from "components/Modals/AddTask";
+
+
+  export default {
+    name: "PageTodo",
+    components: {
+      AddTask,
+      Task
+    },
+    data() {
+      return {
+        showAddTask: false
+      }
+    },
+
+    computed: {
+      ...mapGetters("tasks", ["tasks"])
+    }
+  }
+</script>
+
+<style>
+
+</style>
