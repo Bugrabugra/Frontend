b8ddb8f18070418b2eb0bacff4f6864351fe036e
Bugrabugra <bugraotken@gmail.com>
2021-02-28 13:07:05 +0300

    28022021 - Burcucuma etiket

---
+++ Other Courses/Vue 3 Composition API Tutorial/8 - Creating a Vuex Todo App with full Typescript Support using Vue 3 Composition API in 5 minutes/project/src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  
+</template>
+
+<script>
+  import {useStore} from "vuex";
+
+
+  export default {
+    name: "Test",
+
+    setup() {
+      const store = useStore()
+
+      return {}
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ Other Courses/Vue 3 Composition API Tutorial/8 - Creating a Vuex Todo App with full Typescript Support using Vue 3 Composition API in 5 minutes/project/src/store/getters.ts
@@ -0,0 +1,18 @@
+import {GetterTree} from "vuex";
+import {State} from "@/store/state";
+
+
+export type Getters = {
+  completedCount(state: State): number
+  totalCount(state: State): number
+}
+
+export const getters: GetterTree<State, State> & Getters = {
+  completedCount(state) {
+    return state.items.filter(i => i.completed).length;
+  },
+
+  totalCount(state) {
+    return state.items.length;
+  }
+}
