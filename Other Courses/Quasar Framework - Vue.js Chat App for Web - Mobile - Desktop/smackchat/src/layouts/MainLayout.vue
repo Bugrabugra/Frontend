5ec6301fe4786a0ec113a9257bb1a39962de8cdc
Bugrabugra <bugraotken@gmail.com>
2021-05-03 09:35:33 +0300

    03052021 - Quasar SmackChat

---
+++ Other Courses/Quasar Framework - Vue.js Chat App for Web - Mobile - Desktop/smackchat/src/layouts/MainLayout.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          v-if="$route.fullPath.includes('/chat')"
+          v-go-back.single
+          icon="arrow_back"
+          label="Back"
+          no-caps
+          flat
+          dense
+        />
+        <q-toolbar-title class="absolute-center">
+          {{title}}
+        </q-toolbar-title>
+
+        <q-btn
+          class="absolute-right q-pr-sm"
+          to="/auth"
+          icon="account_circle"
+          label="Login"
+          no-caps
+          flat
+          dense
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+  export default {
+    name: 'MainLayout',
+
+    components: {},
+
+    computed: {
+      title() {
+        const currentPath = this.$route.fullPath;
+
+        if (currentPath === "/") {
+          return "SmackChat";
+        } else if (currentPath === "/chat") {
+          return "Chat";
+        } else if (currentPath === "/auth") {
+          return "Login";
+        }
+      }
+    }
+  }
+</script>
---
+++ Other Courses/Quasar Framework - Vue.js Chat App for Web - Mobile - Desktop/smackchat/src/pages/PageUsers.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-page class="flex q-pa-md">
+    <q-list
+      class="full-width"
+      separator
+    >
+      <q-item
+        v-for="user in users"
+        :key="user.id"
+        to="/chat"
+        clickable
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">
+            {{user.name.charAt(0)}}
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{user.name}}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge :color="user.online ? 'light-green-5' : 'grey-6'">{{user.online ? "Online" : "Offline"}}</q-badge>
+        </q-item-section>
+
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+  export default {
+    name: 'PageIndex',
+
+    data() {
+      return {
+        users: [
+          {
+            id: 1,
+            name: "Ahmet Mehmet",
+            online: true
+          },
+          {
+            id: 2,
+            name: "Buğra Otken",
+            online: false
+          },
+          {
+            id: 3,
+            name: "Cem Aksa",
+            online: true
+          }
+        ]
+      }
+    },
+  }
+</script>
