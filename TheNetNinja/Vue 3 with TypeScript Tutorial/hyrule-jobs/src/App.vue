b16c084b6b92b21dd8fbaf1ceef51ddfc7fca036
Bugrabugra <bugraotken@gmail.com>
2021-05-05 18:01:11 +0300

    05052021 - Sass eğitimi

---
+++ TheNetNinja/Vue 3 with TypeScript Tutorial/hyrule-jobs/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app">
+    <p>{{name}} - {{age}}</p>
+    <button @click="changeName('Zelda')">change name</button>
+    <button @click="changeAge(30)">change age</button>
+  </div>
+</template>
+
+<script lang="ts">
+  import {defineComponent} from 'vue';
+
+  export default defineComponent({
+    name: 'App',
+
+    components: {},
+
+    data() {
+      return {
+        name: "Link",
+        age: 25 as number | string
+      }
+    },
+
+    methods: {
+      changeName(name: string) {
+        this.name = name;
+        return name;
+      },
+
+      changeAge(age: number | string) {
+        this.age = age;
+        return age;
+      }
+    }
+  });
+</script>
