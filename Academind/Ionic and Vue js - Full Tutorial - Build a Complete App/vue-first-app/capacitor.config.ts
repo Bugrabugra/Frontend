f60405b40dd417b8a80ae3c8d79244fd39dc726f
Bugrabugra <bugraotken@gmail.com>
2021-05-26 09:41:17 +0300

    26052021 - Acedemind Ionic

---
+++ Academind/Ionic and Vue js - Full Tutorial - Build a Complete App/vue-first-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vue-first-app',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ Academind/Ionic and Vue js - Full Tutorial - Build a Complete App/vue-first-app/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
