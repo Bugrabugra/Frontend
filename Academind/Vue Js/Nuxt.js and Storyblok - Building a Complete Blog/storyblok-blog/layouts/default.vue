f2d775fbd8a911a3bcc73d889774d4edc6517063
Bugrabugra <bugraotken@gmail.com>
2021-06-03 17:39:25 +0300

    03062021 - Nuxt.js

---
+++ Academind/Vue Js/Nuxt.js and Storyblok - Building a Complete Blog/storyblok-blog/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <TheHeader/>
+    <main class="main-content">
+      <Nuxt/>
+    </main>
+  </div>
+</template>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Lato", sans-serif;
+    margin: 0;
+  }
+
+  .main-content {
+    margin-top: 4.5rem;
+  }
+</style>
