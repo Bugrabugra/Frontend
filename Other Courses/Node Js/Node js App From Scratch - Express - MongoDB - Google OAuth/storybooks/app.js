358f0937613cb73b955b75ddb3a3283bf2065e2a
Bugrabugra <bugraotken@gmail.com>
2021-06-07 17:28:14 +0300

    07062021 - NodeJs Mongo Express Passport

---
+++ Other Courses/Node Js/Node js App From Scratch - Express - MongoDB - Google OAuth/storybooks/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+// Load config
+dotenv.config({
+  path: "./config/config.env"
+});
+
+connectDB();
+
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
