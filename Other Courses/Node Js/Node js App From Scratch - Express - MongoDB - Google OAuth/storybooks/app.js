241ba275be13617f057d465f57a4fc6ef7a566dc
Bugrabugra <bugraotken@gmail.com>
2021-06-08 17:33:38 +0300

    08062021 - NodeJs Mongo Express Passport

--- Other Courses/Node Js/Node js App From Scratch - Express - MongoDB - Google OAuth/storybooks/app.js
+++ Other Courses/Node Js/Node js App From Scratch - Express - MongoDB - Google OAuth/storybooks/app.js
@@ -1,5 +1,12 @@
+const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const connectDB = require("./config/db");
 
 // Load config
@@ -7,9 +14,50 @@ dotenv.config({
   path: "./config/config.env"
 });
 
+// Passport config
+require("./config/passport")(passport);
+
+// Database
 connectDB();
 
-const app = express()
+// App
+const app = express();
+
+// Body parser
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// Logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Handlebars
+app.engine(".hbs", exphbs({defaultLayout: "main", extname: ".hbs"}));
+app.set("view engine", ".hbs");
+
+// Sessions
+app.use(session({
+  secret: "pluton",
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}))
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")))
+
+// Routes
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
+
+// Port
 const PORT = process.env.PORT || 3000;
 
+// Start server
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
