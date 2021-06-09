1c366949f6dbf7d8a42150a212a684b3045f0364
Bugrabugra <bugraotken@gmail.com>
2021-06-09 16:55:26 +0300

    09062021 - NodeJs Mongo Express Passport

--- Other Courses/Node Js/Node js App From Scratch - Express - MongoDB - Google OAuth/storybooks/app.js
+++ Other Courses/Node Js/Node js App From Scratch - Express - MongoDB - Google OAuth/storybooks/app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
 const passport = require("passport");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
@@ -27,13 +28,37 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+// Method override
+app.use(methodOverride((req, res) => {
+  if (req.body && typeof req.body === "object" && "_method" in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}))
+
+
 // Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Handlebars helpers
+const {formatDate, stripTags, truncate, editIcon, select} = require("./helpers/hbs");
+
 // Handlebars
-app.engine(".hbs", exphbs({defaultLayout: "main", extname: ".hbs"}));
+app.engine(".hbs", exphbs({
+  helpers: {
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon,
+    select
+  },
+  defaultLayout: "main",
+  extname: ".hbs"
+}));
 app.set("view engine", ".hbs");
 
 // Sessions
@@ -47,6 +72,12 @@ app.use(session({
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set global var
+app.use(function(req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+})
 
 // Static folder
 app.use(express.static(path.join(__dirname, "public")))
--- Other Courses/Node Js/Node js App From Scratch - Express - MongoDB - Google OAuth/storybooks/routes/stories.js
+++ Other Courses/Node Js/Node js App From Scratch - Express - MongoDB - Google OAuth/storybooks/routes/stories.js
@@ -22,5 +22,130 @@ router.post("/", ensureAuth, async (req, res) => {
   }
 });
 
+// @desc  Show all stories
+// @route GET /stories
+router.get("/", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story
+      .find({status: "public"})
+      .populate("user")
+      .sort({createdAt: "desc"})
+      .lean()
+
+    res.render("stories/index", {
+      stories
+    })
+  } catch (error) {
+    console.error(error);
+    res.render("/error/500");
+  }
+});
+
+// @desc  Show single story
+// @route GET /stories/:id
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story
+      .findById(req.params.id)
+      .populate("user")
+      .lean()
+
+    if (!story) {
+      return res.render("error/404")
+    }
+
+    res.render("stories/show", {
+      story
+    })
+  } catch (error) {
+    console.error(error);
+    res.render("error/404");
+  }
+});
+
+// @desc  Show edit page
+// @route GET /stories/edit/:id
+router.get("/edit/:id", ensureAuth, async (req, res) => {
+  try {
+    const story = await Story.findOne({
+      _id: req.params.id
+    }).lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user !== req.user.id) {
+      res.redirect("/stories");
+    } else {
+      res.render("stories/edit", {
+        story
+      })
+    }
+  } catch (error) {
+    console.error(error);
+    res.render("/error/500");
+  }
+});
+
+// @desc  Update story
+// @route PUT /stories/:id
+router.put("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean()
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user !== req.user.id) {
+      res.redirect("/stories");
+    } else {
+      story = await Story.findOneAndUpdate({_id: req.params.id}, req.body, {
+        new: true,
+        runValidators: true
+      })
+
+      res.redirect("/dashboard");
+    }
+  } catch (error) {
+    console.error(error);
+    return res.ren
+  }
+});
+
+// @desc  Delete story
+// @route DELETE /stories/:id
+router.delete("/:id", ensureAuth, async (req, res) => {
+  try {
+    await Story.remove({_id: req.params.id});
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.error(error);
+    return res.render("error/500");
+  }
+});
+
+// @desc  User stories
+// @route GET /stories/user/:userId
+router.get("/user/:userId", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story
+      .find({
+        user: req.params.userId,
+        status: "public"
+      })
+      .populate("user")
+      .lean()
+
+    res.render("stories/index", {
+      stories
+    })
+  } catch (error) {
+    console.error(error);
+    res.render("error/500");
+  }
+});
+
 module.exports = router;
 
