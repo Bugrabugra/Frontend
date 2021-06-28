6be17db8d64b4d92f140c2e81e5e1fa52cff3b86
Bugrabugra <bugraotken@gmail.com>
2021-06-28 14:07:20 +0300

    28062021 - TraversyMedia
    1- Video landing page
    2- Vue Covid tracker

---
+++ Other Courses/Vue Js/Build a Covid Tracker App With Vue js and Tailwind/vue-covid-tracker/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Header/>
+  <div class="container mx-auto">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import Header from "./components/Header";
+  export default {
+    components: {Header}
+  }
+</script>
---
+++ Other Courses/Vue Js/Build a Covid Tracker App With Vue js and Tailwind/vue-covid-tracker/src/components/DataTitle.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="text-center">
+    <h2 class="text-3xl font-bold">{{text}}</h2>
+    <div class="text-2xl mt-4 mb-10">
+      {{timestamp}}
+    </div>
+  </div>
+</template>
+
+<script>
+  import moment from "moment";
+
+  export default {
+    name: "DataTitle",
+    props: ["text", "dataDate"],
+    data() {
+      return {}
+    },
+    computed: {
+      timestamp() {
+        return moment(this.dataDate).format("MMMM Do YYYY, h:mm:ss a");
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
