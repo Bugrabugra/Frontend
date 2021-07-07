7e13d9b710fba22dfe5845d3340d0ccc56ec8195
Bugrabugra <bugraotken@gmail.com>
2021-07-07 18:15:14 +0300

    07072021 1- Building Workcation with Tailwind CSS

---
+++ Other Courses/CSS/Building Workcation with Tailwind CSS/workcation/tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('./node_modules/tailwindcss/colors');
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        rose: colors.rose,
+        fuchsia: colors.fuchsia,
+        indigo: colors.indigo,
+        teal: colors.teal,
+        lime: colors.lime,
+        orange: colors.orange,
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: 'class',
+    })
+  ],
+}
