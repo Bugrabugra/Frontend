7cf349ec019f84c5ba29e8025e0cc260ea400df2
bugrabugra <bugraotken@gmail.com>
2021-07-19 00:39:54 +0300

    19072021 1- Tailwind CSS Blurry animation

--- Other Courses/CSS/Building Blurry Animated Background Shapes with Tailwind CSS/blurry-blobs/tailwind.config.js
+++ Other Courses/CSS/Building Blurry Animated Background Shapes with Tailwind CSS/blurry-blobs/tailwind.config.js
@@ -1,8 +1,28 @@
 module.exports = {
-  purge: [],
+  purge: ["index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blob: "blob 7s infinite"
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+          }
+        }
+      }
+    },
   },
   variants: {
     extend: {},
