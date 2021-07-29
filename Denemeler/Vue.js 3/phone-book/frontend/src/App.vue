807bdbd8129050236d01d4a671f2542d7fceb241
Bugrabugra <bugraotken@gmail.com>
2021-07-29 20:06:23 +0300

    29072021 1- Phone book Vue 3 TailwindCSS

---
+++ Denemeler/Vue.js 3/phone-book/frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="min-h-screen flex justify-center bg-gradient-to-b from-blue-900 to-blue-500">
+    <div class="max-w-3xl bg-blue-100 flex justify-between w-8/12 h-64 rounded-lg mt-10 shadow-xl">
+      <div class="w-2/3 mx-4 my-4 bg-blue-400 rounded-lg">
+
+      </div>
+
+      <div class="w-1/3 mx-4 my-4 bg-blue-300 rounded-lg">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
