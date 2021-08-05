efbde5d4875aae52adffdea92b228c035077214c
Bugrabugra <bugraotken@gmail.com>
2021-08-05 10:36:33 +0300

    05082021 1- Vue 3, Modal

---
+++ Other Courses/Vue Js/Build a Reusable Modal Component Using Vue 3 - The Composition API - Slots/vue-modal/src/views/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <Modal @close="toggleModal" :modal-active="modalActive">
+      <div class="modal-content">
+        <h1>This is a Modal Header</h1>
+        <p>This is a modal message</p>
+      </div>
+    </Modal>
+
+    <button type="button" @click="toggleModal">Open Modal</button>
+  </div>
+</template>
+
+<script>
+  import {ref} from "vue";
+  import Modal from "../components/Modal";
+
+
+  export default {
+    name: 'Home',
+    components: {Modal},
+    setup() {
+      const modalActive = ref(false);
+
+      const toggleModal = () => {
+        modalActive.value = !modalActive.value;
+      }
+
+      return {modalActive, toggleModal}
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .home {
+    background-color: rgba(0, 176, 234, 0.5);
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal-content {
+    display: flex;
+    flex-direction: column;
+
+    h1, p {
+      margin-bottom: 16px;
+    }
+
+    h1 {
+      font-size: 32px;
+    }
+
+    p {
+      font-size: 18px;
+    }
+  }
+</style>
