be41edef131e0f1d1128071230eb1bd47d2dbaa4
Bugrabugra <bugraotken@gmail.com>
2021-08-16 16:52:59 +0300

    16082021 1- Vue 3, Drag & Drop File

---
+++ Other Courses/Vue Js/Build a DropZone Component With Vue 3/vue-drop/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Varela+Round&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Varela Round', sans-serif;
+  }
+</style>
