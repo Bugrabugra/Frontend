f9de90dc70f469630b725fd9ecbc526240c6305c
Bugrabugra <bugraotken@gmail.com>
2021-09-03 14:35:18 +0300

    03092021 1- Tailwind CSS

---
+++ Other Courses/CSS/Tailwind CSS/Tailwind CSS Dark Mode/dark/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["index.html"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ Other Courses/CSS/Tailwind CSS/Using Tailwind CSS Grid Classes/grid/README.md
@@ -0,0 +1,4 @@
+# Install & Run
+```
+npm i && npx tailwindcss -i ./tailwind/tailwind.css -o ./css/style.css --watch
+```
