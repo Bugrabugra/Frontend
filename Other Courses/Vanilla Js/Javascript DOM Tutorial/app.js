45e40b38530d7799a575cf0b8b256f56bacc02fa
Bugrabugra <bugraotken@gmail.com>
2021-09-09 15:39:19 +0300

    09092021 1- Test

--- Other Courses/Vanilla Js/Javascript DOM Tutorial/app.js
+++
@@ -1,9 +0,0 @@
-const books = document.querySelectorAll("#book-list li .name");
-
-Array.from(books).forEach(book => {
-  book.textContent += " (book title)"
-})
-
-const bookList = document.querySelector("#book-list");
-// bookList.innerHTML = "<h2>Books and more books...</h2>";
-bookList.innerHTML += "<h2>This is how you append</h2>";
