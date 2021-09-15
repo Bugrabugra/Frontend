3f3e4f76a300ebca08c30b03166c13ac2bd512db
Bugrabugra <bugraotken@gmail.com>
2021-09-15 21:31:08 +0300

    15092021 ArcGIS Select by Attributes 1- Vite, Vue 3, Tailwind CSS

--- Denemeler/Vite/arcgis-select-by-attribute/client/src/components/Error.vue
+++ Denemeler/Vite/arcgis-select-by-attribute/client/src/components/Error.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="fixed overflow-x-hidden overflow-y-auto inset-0 flex justify-center items-center z-50">
-    <div class="relative mx-auto w-auto max-w-2xl">
-      <div class="bg-white w-full px-2 py-2 rounded-lg shadow-2xl flex flex-col items-center justify-center">
-        <div class="">
+    <div @click="clickOutside" class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full"></div>
+    <div
+        :class="{'scale-105 transition': outside}"
+        class="relative mx-auto w-auto"
+    >
+      <div class="bg-white w-full px-2 py-2 rounded-lg border-t-[10px] border-teal-300 h-24 shadow-2xl flex flex-col items-center justify-center">
+        <div class="mb-4">
           Error at SQL statement
         </div>
         <button class="button" @click="closeError">
@@ -11,12 +15,11 @@
       </div>
     </div>
   </div>
-
-  <div @click="closeError"  class="absolute inset-0 z-30 opacity-25 bg-black"></div>
 </template>
 
 <script>
   import {useStore} from "vuex";
+  import {ref} from "vue";
 
 
   export default {
@@ -25,12 +28,22 @@
       // store
       const store = useStore();
 
+      // references
+      const outside = ref(false);
+
       // methods
       const closeError = () => {
         store.commit("setError", false);
       };
 
-      return {closeError}
+      const clickOutside = () => {
+        outside.value = true;
+        setTimeout(() => {
+          outside.value = false;
+        }, 200)
+      };
+
+      return {closeError, clickOutside, outside}
     }
   }
 </script>
