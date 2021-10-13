424321d46bbd700c71c4eb1abeac836e82d1a7b6
Bugrabugra <bugraotken@gmail.com>
2021-10-13 13:09:27 +0300

    13102021 1- Vue.js

--- Other Courses/Vue Js/How to Create a Vue Custom Directive/custom-directive/src/App.vue
+++ Other Courses/Vue Js/How to Create a Vue Custom Directive/custom-directive/src/App.vue
@@ -1,21 +1,25 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <p>Default Font Size</p>
+  <p v-font-size="fontSize">Modified Font Size</p>
+  <p v-font-size:small>Small</p>
+  <p v-font-size:medium>Medium</p>
+  <p v-font-size:large.veryLarge.red>Large</p>
+  <button @click="fontSize *= 2">Bigger Font</button>
 </template>
 
+<script setup>
+  import {ref} from "vue";
+
+  const fontSize = ref(8);
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: "Avenir LT Std", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- Other Courses/Vue Js/How to Create a Vue Custom Directive/custom-directive/src/main.js
+++ Other Courses/Vue Js/How to Create a Vue Custom Directive/custom-directive/src/main.js
@@ -1,4 +1,37 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const app = createApp(App);
+
+app.directive("font-size", {
+  // directive = v-font-size
+  beforeMount: (el, binding) => {
+    let size = 12;
+
+    // argument = binding.arg (v-font-size:small)
+    switch (binding.arg) {
+      case "small":
+        size = 8;
+        break;
+      case "large":
+        size = 24;
+        break;
+    };
+
+    if (binding.modifiers.veryLarge) {
+      size = 40
+    }
+
+    if (binding.modifiers.red) {
+      el.style.color = "#ff0000";
+    }
+
+    el.style.fontSize = size + "px";
+  },
+  updated: (el, binding) => {
+    el.style.fontSize = binding.value + "px";
+  },
+})
+
+app.mount('#app')
