ea6e5974adcc7061b975446b56b98ee1599bcf03
Bugrabugra <bugraotken@gmail.com>
2021-10-14 14:49:10 +0300

    14102021 Meteoroloji 1- Ozanın tasarımlar giydirildi 2- GitHub düzeni

--- Other Courses/CSS/CSS Variables Tutorial/Lesson 4/swatches.js
+++
@@ -1,8 +0,0 @@
-const swatches = document.querySelectorAll(".swatches span");
-const root = document.querySelector(":root");
-
-swatches.forEach((swatch) => {
-  swatch.addEventListener("click", (e) => {
-    root.style.setProperty("--theme-color", e.target.style.background);
-  })
-})
