086bd6e27ef35b8f776f584ce567de284649c675
Bugrabugra <bugraotken@gmail.com>
2021-11-19 17:36:02 +0300

    18112021 11 Practice Project_ Building a Food Order App - 141. Managing Cart & Modal State

--- Udemy/React - The Complete Guide (incl Hooks, React Router, Redux)/react-complete-guide-11/src/components/Layout/HeaderCardButton.js
+++ Udemy/React - The Complete Guide (incl Hooks, React Router, Redux)/react-complete-guide-11/src/components/Layout/HeaderCardButton.js
@@ -4,7 +4,7 @@ import CartIcon from "../Cart/CartIcon";
 
 const HeaderCardButton = (props) => {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon/>
       </span>
