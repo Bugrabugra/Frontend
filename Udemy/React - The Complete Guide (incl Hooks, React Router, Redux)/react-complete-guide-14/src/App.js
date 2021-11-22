52ef58539aaefcb60ad1c55d57df5cf4a503ffd6
Bugrabugra <bugraotken@gmail.com>
2021-11-23 01:57:27 +0300

    23112021 Section 14'e geçildi.

---
+++ Udemy/React - The Complete Guide (incl Hooks, React Router, Redux)/react-complete-guide-14/src/App.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMoviesHandler = async () => {
+    setIsLoading(true);
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map(movie => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_Date
+      }
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
+  };
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies}/>}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
