af7f5080760859d2739235666c64abe0d57389fe
Bugrabugra <bugraotken@gmail.com>
2021-12-10 11:32:55 +0300

    10122021 1- React

--- Other Courses/React/Chat Application using React JS - Build and Deploy a Chat App in 1 Hour - Microsoft Teams/chat-app/src/App.js
+++ Other Courses/React/Chat Application using React JS - Build and Deploy a Chat App in 1 Hour - Microsoft Teams/chat-app/src/App.js
@@ -3,18 +3,23 @@ import ChatFeed from "./components/ChatFeed.jsx";
 
 import "./App.css";
 import dotenv from "dotenv";
+import LoginForm from "./components/LoginForm";
  
 
 dotenv.config();
 console.log(process.env.REACT_APP_PROJECTID);
 
 const App = () => {
+  if (!localStorage.getItem("username")) {
+    return <LoginForm/>
+  }
+
   return (
     <ChatEngine
       height="100vh"
       projectID={process.env.REACT_APP_PROJECTID}
-      userName="bugra"
-      userSecret="123"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={chatAppProps => <ChatFeed {...chatAppProps}/>}
     />
   )
--- Other Courses/React/Chat Application using React JS - Build and Deploy a Chat App in 1 Hour - Microsoft Teams/chat-app/src/components/MyMessage.jsx
+++ Other Courses/React/Chat Application using React JS - Build and Deploy a Chat App in 1 Hour - Microsoft Teams/chat-app/src/components/MyMessage.jsx
@@ -1,7 +1,25 @@
-const MyMessage = () => {
+const MyMessage = ({message}) => {
+  if (message?.attachments?.length > 0) {
+    return (
+      <img 
+        src={message.attachments[0].file} 
+        alt="message-attachment" 
+        className="message-image"
+        style={{float: "right"}}
+      />
+    )
+  }
+
   return (
-    <div>
-      MyMessage
+    <div 
+      className="message" 
+      style={{
+        float: "right", 
+        marginRight: "18px", 
+        color: "white", 
+        backgroundColor: "#3b2a50"
+      }}>
+      {message.text}
     </div>
   )
 }
