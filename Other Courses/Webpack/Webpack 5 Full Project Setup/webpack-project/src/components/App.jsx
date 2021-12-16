3b6876208f0644c35d051a2612d8cb5e01628151
Bugrabugra <bugraotken@gmail.com>
2021-12-16 11:40:22 +0300

    added dit folder support

--- Other Courses/Webpack/Webpack 5 Full Project Setup/webpack-project/src/components/App.jsx
+++ Other Courses/Webpack/Webpack 5 Full Project Setup/webpack-project/src/components/App.jsx
@@ -1,6 +1,8 @@
 import "../styles/index.scss";
 import React from 'react'
 import Recipes from "./Recipes";
+import sword from "../images/swc-sword.png";
+import swordSvg from "../images/sword.svg";
 
 
 const App = () => {
@@ -13,9 +15,10 @@ const App = () => {
         <section>
           <h1>Oh Hai, React</h1>
         </section>
-      </main>
-      
-      <Recipes/>
+        <img src={sword} alt="sword" width="250"/>
+        <img src={swordSvg} alt="sword" width="250"/>
+        <Recipes/>  
+      </main> 
     </>
   )
 }
--- Other Courses/Webpack/Webpack 5 Full Project Setup/webpack-project/webpack.config.js
+++ Other Courses/Webpack/Webpack 5 Full Project Setup/webpack-project/webpack.config.js
@@ -1,8 +1,10 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const path = require("path");
 
 let mode = "development";
-let target = "web"
+let target = "web";
 
 if (process.env.NODE_ENV === "production") {
   mode = "production";
@@ -12,22 +14,44 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   mode: mode,
   target: target,
+  output: {
+    // to be able to clear dist folder, you have to define the output folder
+    path: path.resolve(__dirname, "dist"),
+    // to create a folder named images and put the images inside
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
   devtool: "source-map",
   devServer: {
     static: {
       directory: path.join(__dirname, "dist"),
     },
-    hot: true
+    hot: true, // hot module replacement
   },
   module: {
     rules: [
       {
-        test: /\.(s[ac]|c)ss$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        // asset/resource: creates folder, put all images inside,
+        // asset/inline: binds images to js inline,
+        // asset: big images to folder, small images to inline
+        type: "asset",
+        // to limit the max file size
+        // parser: {
+        //   dataUrlCondition: {
+        //     maxSize: 30 * 1024
+        //   }
+        // }
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i, // to be able to handle css, scss, sass
         use: [
-          MiniCssExtractPlugin.loader, 
-          "css-loader", 
-          "postcss-loader", 
-          "sass-loader"
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "" },
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
         ],
       },
       {
@@ -39,8 +63,14 @@ module.exports = {
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(), // clears dist folder
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }), // creates html file in dist folder
+  ],
   resolve: {
-    extensions: [".js", ".jsx"]
-  }
+    extensions: [".js", ".jsx"], // to handle file extensions
+  },
 };
