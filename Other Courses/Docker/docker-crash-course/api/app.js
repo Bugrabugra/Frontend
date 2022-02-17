a42ffe7a213a245f911e9eac496b4eb681c7c006
Buğra <bugraotken@gmail.com>
2022-02-17 11:02:01 +0300

    docker

--- Other Courses/Docker/docker-crash-course/api/app.js
+++ Other Courses/Docker/docker-crash-course/api/app.js
@@ -8,19 +8,19 @@ app.get('/', (req, res) => {
   res.json([
     {
       "id":"1",
-      "title":"Book Review: The Bear & The Nightingale"
+      "title":"Book Review: The Bear & The Nightingale..."
     },
     {
       "id":"2",
-      "title":"Game Review: Pokemon Brilliant Diamond"
+      "title":"Game Review: Pokemon Brilliant Diamond..."
     },
     {
       "id":"3",
-      "title":"Show Review: Alice in Borderland"
+      "title":"Show Review: Alice in Borderland..."
     }
   ])
 })
 
 app.listen(4000, () => {
-  console.log('listening for requests on port 4000')
+  console.log('listening for requests on port 4000...')
 })
