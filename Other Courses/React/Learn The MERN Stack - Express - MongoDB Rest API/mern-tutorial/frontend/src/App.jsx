79b4a199939917b7f883fb19172eafb9a030700d
Buğra <bugraotken@gmail.com>
2022-02-23 17:31:14 +0300

    MERN

--- Other Courses/React/Learn The MERN Stack - Express - MongoDB Rest API/mern-tutorial/frontend/src/App.jsx
+++ Other Courses/React/Learn The MERN Stack - Express - MongoDB Rest API/mern-tutorial/frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
