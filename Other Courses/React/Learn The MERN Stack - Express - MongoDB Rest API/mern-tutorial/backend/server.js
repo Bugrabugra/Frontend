f552d19b2d4c1fd8bcf2ca1ad5f8ad66d6d0d889
Buğra <bugraotken@gmail.com>
2022-02-28 12:00:15 +0300

    mern stack

--- Other Courses/React/Learn The MERN Stack - Express - MongoDB Rest API/mern-tutorial/backend/server.js
+++ Other Courses/React/Learn The MERN Stack - Express - MongoDB Rest API/mern-tutorial/backend/server.js
@@ -3,6 +3,7 @@ const colors = require("colors");
 const cors = require("cors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const { connectDB } = require("./config/db");
+const path = require("path");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 const whitelist = ["http://localhost:3000"];
@@ -26,6 +27,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+// serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  console.log(path.join(__dirname, "../frontend/dist"));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "dist", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
