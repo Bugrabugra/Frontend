71518a5c6a33e933e2434939d517cd7712c5f90a
Bugrabugra <bugraotken@gmail.com>
2022-03-01 02:23:17 +0300

    Cryptocurrency React

---
+++ Other Courses/React/Build and Deploy a React Cryptocurrency App and Master Redux Toolkit in One Video/cryptoverse/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Navbar } from "./components";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+
+      </div>
+      <div className="footer">
+
+      </div>
+    </div>
+  );
+};
+
+export default App;
