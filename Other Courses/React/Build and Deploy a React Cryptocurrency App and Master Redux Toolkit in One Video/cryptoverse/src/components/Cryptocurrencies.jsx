24906572c0720947cb9e65399de6a50edf570774
Buğra <bugraotken@gmail.com>
2022-03-04 18:19:45 +0300

    crypto ant design

--- Other Courses/React/Build and Deploy a React Cryptocurrency App and Master Redux Toolkit in One Video/cryptoverse/src/components/Cryptocurrencies.jsx
+++ Other Courses/React/Build and Deploy a React Cryptocurrency App and Master Redux Toolkit in One Video/cryptoverse/src/components/Cryptocurrencies.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import { Card, Col, Row } from "antd";
+import { Link } from "react-router-dom";
+import millify from "millify";
 
 
 const Cryptocurrencies = () => {
+  const { data: cryptoList, isFetching } = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(cryptoList?.data?.coins);
+
+  console.log(cryptos);
   return (
-    <div>
-      Crptocurrencies
-    </div>
+    <>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos.map(currency => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link to={`/crypto/${currency.id}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img src={currency.iconUrl} className="crypto-image" alt=""/>}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 };
 
