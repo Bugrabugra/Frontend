b97bd45fdd0e638835689381372c65f2e736103d
Bugrabugra <bugraotken@gmail.com>
2022-03-17 19:32:58 +0300

    wordle

---
+++ Other Courses/React/Build a Wordle Clone in ReactJS - Best React Project/wordle/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useState } from "react";
+import "./App.css";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { boardDefault } from "./components/Words";
+
+export const AppContext = createContext();
+
+function App() {
+  const [board, setBoard] = useState(boardDefault);
+
+  return (
+    <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+
+      <AppContext.Provider value={{ board, setBoard }}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
+    </div>
+  );
+}
+
+export default App;
