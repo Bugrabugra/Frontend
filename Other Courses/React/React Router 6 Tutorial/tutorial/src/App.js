0db198a4c5947d7f77ce97633ba08d1b9ce34c1b
Bugrabugra <bugraotken@gmail.com>
2022-03-28 12:09:48 +0300

    react router 6

---
+++ Other Courses/React/React Router 6 Tutorial/tutorial/src/App.js
@@ -0,0 +1,35 @@
+import { BrowserRouter, Link, Redirect, Route, Routes, Navigate } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
+import ProductDetails from './pages/ProductDetails'
+import { useState } from "react";
+
+function App() {
+  const [cartIsEmpty, setCartIsEmpty] = useState(false);
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <nav>
+          <h1>The Ninja Clothing Company</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about/*" element={<About/>}/>
+          <Route path="/products/:id/*" element={<ProductDetails/>} />
+          <Route path="/products" element={<Products />}/>
+          <Route path="/redirect" element={<Navigate to="/about" />}/>
+          <Route path="/checkout" element={cartIsEmpty ? <Navigate to="/products" /> : <p>checkout</p>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ Other Courses/React/React Router 6 Tutorial/tutorial/src/pages/ProductDetails.js
@@ -0,0 +1,32 @@
+import { Route, useParams, Routes } from "react-router-dom"
+
+// nested routes
+import Offers from "./Offers"
+
+export default function ProductDetails() {
+  const { id } = useParams()
+  // const { path } = useRouteMatch()
+
+  return (
+    <div className="content">
+      <div className="product">
+        <div className="image">
+          <img src="https://via.placeholder.com/520x460" alt="" />
+        </div>
+        <div className="details">
+          <h2>Product - {id}</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos eaque repudiandae itaque dolorem nihil, voluptas corporis tempora provident optio harum modi inventore esse nostrum exercitationem magnam tempore odio laborum velit! Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi voluptate neque harum. Quam facere accusamus exercitationem in quidem mollitia eligendi porro eos voluptates iure incidunt, laudantium sed harum omnis quasi?</p>
+        </div>
+      </div>
+
+      {/*<Route path={`${path}/offers`}>*/}
+      {/*  <Offers />*/}
+      {/*</Route>*/}
+
+      <Routes>
+        <Route path="offers" element={<Offers />}/>
+      </Routes>
+    </div>
+  )
+}
