7ac60dbe2d4f08ac54d4ab5d9cd79b5e5129f6d2
Bugrabugra <bugraotken@gmail.com>
2022-06-10 00:10:27 +0300

    graphql

--- Other Courses/Others/GraphQL Crash Course With Full Stack MERN Project/project-management-app/server/index.js
+++ Other Courses/Others/GraphQL Crash Course With Full Stack MERN Project/project-management-app/server/index.js
@@ -1,14 +1,22 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
+const colors = require("colors");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use("/graphql", graphqlHTTP({
-  schema,
-  graphiql: process.env.NODE_ENV === "development"
-}));
+// connect to database
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development"
+  })
+);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ Other Courses/Others/GraphQL Crash Course With Full Stack MERN Project/project-management-app/server/schema/schema.js
@@ -0,0 +1,112 @@
+// mongoose models
+const Project = require("../models/Project");
+const Client = require("../models/Client");
+
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
+} = require("graphql");
+
+// client type
+const ClientType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString }
+  })
+});
+
+// projects type
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return Client.findById(parent.clientId);
+      }
+    }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return Project.find();
+      }
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Project.findById(args.id);
+      }
+    },
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return Client.find();
+      }
+    },
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Client.findById(args.id);
+      }
+    }
+  }
+});
+
+// mutations
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    // add a client
+    addClient: {
+      type: ClientType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        email: { type: GraphQLNonNull(GraphQLString) },
+        phone: { type: GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parent, args) {
+        const client = new Client({
+          name: args.name,
+          email: args.email,
+          phone: args.phone
+        });
+
+        return client.save();
+      }
+    },
+    // delete a client
+    deleteClient: {
+      type: ClientType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) }
+      },
+      resolve(parent, args) {
+        return Client.findByIdAndRemove(args.id);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation
+});
