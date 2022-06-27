fbf2461a3c981b30a02540a8084a5ac185f43576
Bugrabugra <bugraotken@gmail.com>
2022-06-27 21:25:48 +0300

    electron-react, React.memo, MERN

---
+++ Other Courses/Others/Electron/How to build a Markdown editor using Electron - ReactJS - Vite - CodeMirror and Remark/vite-electron-builder-main/packages/renderer/vite.config.js
@@ -0,0 +1,43 @@
+/* eslint-env node */
+
+import {chrome} from '../../.electron-vendors.cache.json';
+import {join} from 'path';
+
+const PACKAGE_ROOT = __dirname;
+
+/**
+ * @type {import('vite').UserConfig}
+ * @see https://vitejs.dev/config/
+ */
+const config = {
+  mode: process.env.MODE,
+  root: PACKAGE_ROOT,
+  resolve: {
+    alias: {
+      '/@/': join(PACKAGE_ROOT, 'src') + '/',
+    },
+  },
+  base: '',
+  server: {
+    fs: {
+      strict: true,
+    },
+  },
+  build: {
+    sourcemap: true,
+    target: `chrome${chrome}`,
+    outDir: 'dist',
+    assetsDir: '.',
+    rollupOptions: {
+      input: join(PACKAGE_ROOT, 'index.html'),
+    },
+    emptyOutDir: true,
+    brotliSize: false,
+  },
+  test: {
+    environment: 'happy-dom',
+  },
+  plugins: [],
+};
+
+export default config;
---
+++ Other Courses/Others/Electron/How to build a Markdown editor using Electron - ReactJS - Vite - CodeMirror and Remark/vite-electron-builder-main/prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+	arrowParens: "avoid",
+	singleQuote: false,
+	bracketSpacing: true,
+	endOfLine: "lf",
+	semi: true,
+	tabWidth: 2,
+	trailingComa: "none"
+}
+
+module.exports = options;
