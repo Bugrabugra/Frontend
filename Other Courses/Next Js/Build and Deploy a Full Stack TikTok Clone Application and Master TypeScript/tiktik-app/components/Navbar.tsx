892d17aa3922d11b34e6eb2e16f945f505ef2f53
Bugrabugra <bugraotken@gmail.com>
2022-07-04 15:12:49 +0300

    next.js sanity.io tailwindcss zustand typescript

---
+++ Other Courses/Next Js/Build and Deploy a Full Stack TikTok Clone Application and Master TypeScript/tiktik-app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../utils/tiktik-logo.png";
+
+const Navbar = () => {
+  return (
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="/">
+        <div className="w-[100px] md:w-[130px]">
+          <Image className="cursor-pointer" src={Logo} alt="TikTik" layout="responsive"/>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ Other Courses/Next Js/Build and Deploy a Full Stack TikTok Clone Application and Master TypeScript/tiktik-app/components/SuggestedAccounts.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SuggestedAccounts = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default SuggestedAccounts;
