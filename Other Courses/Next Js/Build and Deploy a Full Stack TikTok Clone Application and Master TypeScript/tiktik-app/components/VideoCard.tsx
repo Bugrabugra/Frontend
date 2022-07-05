e7a56f918af9bf53ef09d3fcd199ffd777383391
Bugrabugra <bugraotken@gmail.com>
2022-07-06 02:31:43 +0300

    next.js sanity.io tailwindcss zustand typescript

---
+++ Other Courses/Next Js/Build and Deploy a Full Stack TikTok Clone Application and Master TypeScript/tiktik-app/components/VideoCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Video } from "../types";
+import { NextPage } from "next";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
+import Link from "next/link";
+import Image from "next/image";
+
+
+interface IProps {
+  post: Video;
+}
+
+const VideoCard: NextPage<IProps> = ({ post }) => {
+  return (
+    <div className="flex flex-col border-b-2 border-gray-200 pb-6">
+      <div className="">
+        <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+          <div className="md:w-16 md:h-16 w-10 h-10">
+            <Link href="/">
+              <>
+                <Image
+                  width={62}
+                  height={62}
+                  className="rounded-full"
+                  src={post.postedBy.image}
+                  alt="profile photo"
+                  layout="responsive"
+                />
+              </>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
--- Other Courses/Next Js/Build and Deploy a Full Stack TikTok Clone Application and Master TypeScript/tiktik-app/pages/index.tsx
+++ Other Courses/Next Js/Build and Deploy a Full Stack TikTok Clone Application and Master TypeScript/tiktik-app/pages/index.tsx
@@ -1,11 +1,36 @@
 import type { NextPage } from "next";
+import axios from "axios";
+import { Video } from "../types";
+import VideoCard from "../components/VideoCard";
+import NoResults from "../components/NoResults";
 
-const Home: NextPage = () => {
+interface IProps {
+  videos: Video[]
+}
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
   return (
-    <div className="text-3xl font-bold underline">
-      Hello World!
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ?
+        videos.map((video) => (
+          <VideoCard post={video} key={video._id}/>
+        )) : (
+          <NoResults text="No Videos"/>
+        )
+      }
     </div>
   );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get("http://localhost:3000/api/post");
+
+  return {
+    props: {
+      videos: data
+    }
+  };
 };
 
 export default Home;
