14773b5668cf4e63da0d5b033a9376dcb321e2fb
Bugrabugra <bugraotken@gmail.com>
2022-07-25 01:20:08 +0300

    google calendar

--- Other Courses/CSS/Tailwind CSS/Modern Landing Page Tailwind CSS Project/homesmart-landing-page/tailwind.config.js
+++ Other Courses/CSS/Tailwind CSS/Modern Landing Page Tailwind CSS Project/homesmart-landing-page/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     ".src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {
+      maxWidth: {
+        "16": "16rem"
+      }
+    }
   },
   plugins: [
     require("@tailwindcss/forms")
