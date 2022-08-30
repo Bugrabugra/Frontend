71bff121abe9d95c41c59c8c1b3ef6c273401948
Bugrabugra <bugraotken@gmail.com>
2022-08-31 01:50:39 +0300

    typescript

---
+++ Other Courses/Typescript/Intro to Typescript/intro-to-typescript-main/example.ts
@@ -0,0 +1,43 @@
+// Type Annotations
+// let myName: string = "CJ";
+// let isCool: boolean = true;
+// let favNumber: number = 42;
+//
+// function add(a: number, b: number) {
+//   return a + b;
+// }
+//
+// console.log(add(4, 2));
+
+// Inferred Types
+let myName = "CJ";
+let isCool: boolean = true;
+let favNumber: number = 42;
+
+// myName = 42;
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+let result = add(40, 2);
+console.log(result);
+
+console.log(add(4, 2));
+
+
+// Union Types
+
+// Objects and Interfaces
+
+// Optional Properties
+
+// Classes
+
+// Generics
+
+// any, unknown, never
+
+// Type Assertions
+
+// @ts-ignore / @ts-nocheck
