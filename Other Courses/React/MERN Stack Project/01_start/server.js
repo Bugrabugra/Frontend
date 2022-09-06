19970e2efe763750690c5ac04fa97e51bc546efd
Bugrabugra <bugraotken@gmail.com>
2022-09-07 01:41:26 +0300

    typescript test & MERN stack

--- Other Courses/React/MERN Stack Project/01_start/server.js
+++ Other Courses/React/MERN Stack Project/01_start/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3500;
 
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
 app.all("*", (req, res) => {
   res.status(404);
