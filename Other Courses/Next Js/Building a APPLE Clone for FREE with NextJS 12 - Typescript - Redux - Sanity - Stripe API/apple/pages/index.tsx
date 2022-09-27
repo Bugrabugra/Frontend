556e7367a373fdf1261496553861f136a083b83b
Bugrabugra <bugraotken@gmail.com>
2022-09-28 02:27:49 +0300

    apple

---
+++ Other Courses/Next Js/Building a APPLE Clone for FREE with NextJS 12 - Typescript - Redux - Sanity - Stripe API/apple/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Apple Redesign</title>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+
+      <h1>Lets build apple</h1>
+    </div>
+  )
+}
+
+export default Home
