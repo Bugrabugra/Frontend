b7abc303175cd6db1691e143c68d326f6ddc970e
Bugrabugra <bugraotken@gmail.com>
2022-09-28 01:52:04 +0300

    mern & tailwind

--- Other Courses/React/MERN Stack Project/02_middleware/server.js
+++ Other Courses/React/MERN Stack Project/02_middleware/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
+
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
@@ -16,5 +26,7 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 Not Found");
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
