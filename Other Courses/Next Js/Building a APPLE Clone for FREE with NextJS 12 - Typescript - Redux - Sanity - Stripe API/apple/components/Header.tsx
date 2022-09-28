27a08e260c9d542ea7dc249d8efd34e19c9a69b3
Bugrabugra <bugraotken@gmail.com>
2022-09-29 01:49:18 +0300

    next

---
+++ Other Courses/Next Js/Building a APPLE Clone for FREE with NextJS 12 - Typescript - Redux - Sanity - Stripe API/apple/components/Header.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+// import {
+//   SearchIcon,
+//   ShoppingBagIcon,
+//   UserIcon,
+// } from "@heroicons/react/outline";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-[#E7ECEE] p-4">
+      <div className="flex items-center justify-center md:w-1/5">
+        <Link href="/">
+          <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
+            <Image
+              src="https://rb.gy/vsvv2o"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+        </Link>
+      </div>
+
+      <div className="hidden flex-1 items-center justify-center space-x-8 md:flex">
+        <a className="headerLink">Product</a>
+        <a className="headerLink">Explore</a>
+        <a className="headerLink">Support</a>
+        <a className="headerLink">Business</a>
+      </div>
+
+      <div className="">
+        {/*<SearchIcon className="headerLink" />*/}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- Other Courses/Next Js/Building a APPLE Clone for FREE with NextJS 12 - Typescript - Redux - Sanity - Stripe API/apple/pages/index.tsx
+++ Other Courses/Next Js/Building a APPLE Clone for FREE with NextJS 12 - Typescript - Redux - Sanity - Stripe API/apple/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Header from "../components/Header";
 
 
 const Home: NextPage = () => {
@@ -10,7 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico"/>
       </Head>
 
-      <h1>Lets build apple</h1>
+      <Header/>
     </div>
   )
 }
