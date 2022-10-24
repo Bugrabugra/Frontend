d6adbb7a7faa08426a6f37e4a4301fa421eef78b
Buğra <bugraotken@gmail.com>
2022-10-24 23:37:12 +0300

    typescript

---
+++ Other Courses/Typescript/Typescript for Beginners/02_basic_types/src/main.ts
@@ -0,0 +1,18 @@
+let myName: string = "Dave";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: string | number;
+
+myName = "John";
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+
+const sum = (a: number, b: string) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /|w+/g;
