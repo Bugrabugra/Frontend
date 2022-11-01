0ad331800d657568eac30bbc58eb6a956300410c
Buğra <bugraotken@gmail.com>
2022-11-01 16:31:13 +0300

    astro

--- Other Courses/Others/Astro/Astro Crash Course/astro-website/astro.config.mjs
+++ Other Courses/Others/Astro/Astro Crash Course/astro-website/astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig();
