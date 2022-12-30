2a0863c5b7ace6a6a299703576afcbf9f504adab
Buğra <bugraotken@gmail.com>
2022-12-31 02:08:24 +0300

    ts

---
+++ Other Courses/Typescript/Typescript for Beginners/13_react_hooks/src/App.tsx
@@ -0,0 +1,59 @@
+import {
+  useCallback,
+  useEffect,
+  useState,
+  MouseEvent,
+  KeyboardEvent,
+  useMemo,
+  useRef
+} from "react";
+
+interface User {
+  id: number,
+  username: string
+}
+
+type fibFunc = (n: number) => number;
+
+const fib: fibFunc = (n) => {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+};
+
+const myNum: number = 37;
+
+function App() {
+  const [count, setCount] = useState<number>(0);
+  const [users, setUsers] = useState<User[] | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null!);
+
+  console.log(inputRef?.current);
+  console.log(inputRef?.current?.value);
+
+  useEffect(() => {
+    console.log("mounting");
+    console.log("Users: ", users);
+
+    return () => console.log("unmounting");
+  }, [users]);
+
+  const addTwo = useCallback(
+    (e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>): void => setCount(prev => prev + 2),
+    []
+  );
+
+  const result = useMemo<number>(() => fib(myNum), [myNum]);
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={addTwo}>Add</button>
+      <h2>{result}</h2>
+      <input type="text" ref={inputRef}/>
+    </div>
+  )
+}
+
+export default App
---
+++ Other Courses/Typescript/Typescript for Beginners/14_useReducer/src/Counter.tsx
@@ -0,0 +1,58 @@
+import React, { ChangeEvent, ReactNode, useReducer, useState } from 'react';
+
+const initialState = { count: 0, text: "" };
+
+const enum REDUCER_ACTION_TYPE {
+  INCREMENT,
+  DECREMENT,
+  NEW_INPUT
+};
+
+type ReducerAction = {
+  type: REDUCER_ACTION_TYPE,
+  payload?: string
+}
+
+const reducer = (state: typeof initialState, action: ReducerAction): typeof initialState => {
+  switch (action.type) {
+    case REDUCER_ACTION_TYPE.INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case REDUCER_ACTION_TYPE.DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case REDUCER_ACTION_TYPE.NEW_INPUT:
+      return { ...state, text: action.payload ?? "" };
+    default:
+      throw new Error();
+  }
+};
+
+type ChildrenType = {
+  children: (num: number) => ReactNode
+}
+
+const Counter = ({ children }: ChildrenType) => {
+  // const [count, setCount] = useState<number>(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // const increment = () => setCount(prev => prev + 1);
+  // const decrement = () => setCount(prev => prev - 1);
+  const increment = () => dispatch({ type: REDUCER_ACTION_TYPE.INCREMENT });
+  const decrement = () => dispatch({ type: REDUCER_ACTION_TYPE.DECREMENT });
+  const handleTextInput = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: REDUCER_ACTION_TYPE.NEW_INPUT, payload: e.target.value });
+  };
+
+  return (
+    <>
+      <h1>{children(state.count)}</h1>
+      <div>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
+      <input type="text" onChange={handleTextInput}/>
+      <h2>{state.text}</h2>
+    </>
+  );
+};
+
+export default Counter;
