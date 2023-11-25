5a9ca22339d877473d15ebbd07f11c75654f9197
Buğra <bugraotken@gmail.com>
2023-11-25 22:10:01 +0300

    next

---
+++ Other Courses/Typescript/Typescript Masterclass - netninja/1-typescript-basics/src/index.ts
@@ -0,0 +1,23 @@
+let age = 30
+let firstName: string = "Mario"
+let isFictional: boolean
+
+age = 31
+firstName = "Luigi"
+isFictional = true
+
+let planet = "Earth"
+let moons = 1
+let isLarge = false
+
+planet = "Saturn"
+moons = 145
+isLarge = true
+
+let something: null
+let anotherThing: undefined
+
+something = null
+anotherThing = undefined
+
+
