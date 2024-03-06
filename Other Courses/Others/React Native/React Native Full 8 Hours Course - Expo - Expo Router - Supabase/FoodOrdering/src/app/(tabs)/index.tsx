11d10168085725d7a2e2434504016ad0f016706f
Buğra <bugraotken@gmail.com>
2024-03-06 18:47:46 +0300

    react native

---
+++ Other Courses/Others/React Native/React Native Full 8 Hours Course - Expo - Expo Router - Supabase/FoodOrdering/src/app/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { FlatList } from "react-native";
+
+import products from "../../../assets/data/products";
+
+import ProductListItem from "@/components/ProductListItem";
+import { View } from "@/components/Themed";
+
+export default function MenuScreen() {
+  return (
+    <View>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        columnWrapperStyle={{ gap: 10 }}
+      />
+    </View>
+  );
+}
---
+++ Other Courses/Others/React Native/React Native Full 8 Hours Course - Expo - Expo Router - Supabase/FoodOrdering/src/components/ProductListItem.tsx
@@ -0,0 +1,54 @@
+import { Link } from "expo-router";
+import { Image, Pressable, StyleSheet } from "react-native";
+
+import { Text } from "@/components/Themed";
+import Colors from "@/constants/Colors";
+import { Product } from "@/types";
+
+export const DEFAULT_PIZZA_IMAGE =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <Link href={`/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product.image || DEFAULT_PIZZA_IMAGE }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+    </Link>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%"
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold"
+  }
+});
+
+export default ProductListItem;
