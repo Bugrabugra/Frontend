0dce06489db7e5df06c1f263985227dc25b94404
Buğra <bugraotken@gmail.com>
2024-03-08 18:55:10 +0300

    before orders

--- Other Courses/Others/React Native/React Native Full 8 Hours Course - Expo - Expo Router - Supabase/FoodOrdering/src/app/index.tsx
+++ Other Courses/Others/React Native/React Native Full 8 Hours Course - Expo - Expo Router - Supabase/FoodOrdering/src/app/index.tsx
@@ -13,6 +13,10 @@ const index = () => {
       <Link href="/(admin)" asChild>
         <Button text="Admin" />
       </Link>
+
+      <Link href="/sign-in" asChild>
+        <Button text="Sign in" />
+      </Link>
     </View>
   );
 };
